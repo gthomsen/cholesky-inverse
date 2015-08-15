c893fece074e07828bf6c7fd013c6bd5d86f080f
Greg Thomsen <gthomsen@gmail.com>
2015-08-15 12:00:18 -0500

    Topic: Cholesky Inverse MEX Object Regression Test and Benchmarking Functions Update Description:   * Updated the documentation for the regression test and benchmark functions.     Now both test_cholesky_inverse() and benchmark_cholesky_inverse() have     help that matches their behavior.
      * Updated the regression test so its coverage is larger and the coding style
        is more consistent.  Changes made include:
    
          - It no longer has dead code.  Specifically computation of random
            integral vectors was removed since they are not used in the tests.
    
          - Added support for int64 matrices.
    
          - Added test_success() and test_failure() as wrappers around run_test()
            so that regression tests that are expected to fail may be tested in
            the same manner as those that are expected to succeed.
    
          - Added test cases for verifying the MEX object refuses to invert
            matrices with integral data types.
    
          - The regression test no longer prints success or halts in a keyboard at
            the end of the tests.
    
      * Updated the benchmark so that it tests symmetric matrices.  Previously, only
        Hermitian matrices were tested.

--- benchmark_cholesky_inverse.m
+++ benchmark_cholesky_inverse.m
@@ -2,7 +2,17 @@ function timings = benchmark_cholesky_inverse( N, number_iterations )
 % timings = benchmark_cholesky_inverse( N, number_iterations )
 %
 % Benchmarks the Cholesky inverse MEX object against other alternatives for
-% computing the explicit inverse within Matlab.
+% computing the explicit inverse within MATLAB and Octave.  Each of the
+% following inversion techniques are run a number of times and the median
+% time for each technique is recorded:
+%
+%   1. Inversion via inv().
+%   2. Inversion via heuristic derived factorization and back substitution.
+%   3. Inversion via Cholesky factorization and back substitution.
+%   4. Inversion via the cholesky_inverse() MEX object.
+%
+% Timings are taken for single and double precision inputs, as well as
+% Hermitian and symmetric matrices, resulting in four sets of timings.
 %
 % Takes 2 arguments:
 %
@@ -13,8 +23,11 @@ function timings = benchmark_cholesky_inverse( N, number_iterations )
 %
 % Returns 1 value:
 %
-%   timings -
-%
+%   timings - Matrix, sized N x 4, of median timings in seconds.  Columns one
+%             and two represents times for each of the N inversion techniques
+%             using double precision data for Hermitian and symmetric
+%             matrices, respectively, while the third and fourth columns
+%             represents timings for single precision data.
 
 if nargin < 2
    number_iterations = [];
@@ -32,32 +45,56 @@ if isempty( number_iterations )
    number_iterations = 20;
 end
 
+% create a full rank, positive definite Hermitian matrix as well as its symmetric
+% counterpart.
 X = randn( N ) + randn( N )*i;
-I = zeros( N );
 R = X * X';
+S = real( R );
+I = zeros( N );
 
-t_double = time_inversion_methods( R, I, number_iterations );
-t_single = time_inversion_methods( single( R ), single( I ), number_iterations );
+% benchmark each data type and matrix type.
+t_double_hermitian = time_inversion_methods( R, I, number_iterations );
+t_double_symmetric = time_inversion_methods( S, I, number_iterations );
+t_single_hermitian = time_inversion_methods( single( R ), single( I ), number_iterations );
+t_single_symmetric = time_inversion_methods( single( S ), single( I ), number_iterations );
 
-timings = [t_double, t_single];
+% pack things up for the caller.
+timings = [t_double_hermitian, t_double_symmetric, t_single_hermitian, t_single_symmetric];
 
 return
 
 function timings = time_inversion_methods( R, I, number_iterations )
+% timings = time_inversion_methods( R, I, number_iterations )
+%
+% Benchmarks the several inversion methods for the specified positive definite
+% matrix.  Each method is performed a caller-specified number of times and the
+% median of all run-times is returned for the method.  The following methods
+% are timed:
+%
+%   1. Inversion via inv().
+%   2. Inversion via heuristic derived factorization and back substitution.
+%   3. Inversion via Cholesky factorization and back substitution.
+%   4. Inversion via the cholesky_inverse() MEX object.
+%
+% Takes 3 arguments:
+%
+%   R                 - Positive definite matrix to invert.
+%   I                 - Identity matrix of the same size and class as R.
+%   number_iterations - Number of iterations to perform the inversion for
+%                       benchmarking purposes.
+%
+% Returns 1 value:
+%
+%   timings - Vector, of size 4 x 1, containing the timings, in seconds, of
+%             each technique.  The ith entry corresponds to the ith technique
+%             listed above.
 
-% timings hold times for:
-%
-%   1. naive inverse
-%   2. hereustic derived factorization + back substitution
-%   3. Cholesky factorization + back substitution
-%   4. Cholesky inverse using MEX
-%
 timings = zeros( 4, 1 );
 
 % individual run timings.  these are condensed down into a scalar value.
 local_timings = zeros( number_iterations, 1 );
 
-% naive inverse.
+% naive inverse.  turn off warnings about nearly singular matrices.
 W = warning( 'off' );
 for iteration_index = 1:number_iterations
     tic
@@ -67,7 +104,8 @@ end
 timings(1) = median( local_timings );
 warning( W );
 
-% factorization and back substitution inverse.
+% factorization and back substitution inverse.  turn off warnings about nearly
+% singular matrices.
 W = warning( 'off' );
 for iteration_index = 1:number_iterations
     tic
--- test_cholesky_inverse.m
+++ test_cholesky_inverse.m
@@ -14,27 +14,31 @@ function status = test_cholesky_inverse()
 %            true otherwise.
 %
 
+% logical indicating whether the MEX object is working according to
+% specification.  if any test fails, we set this to false.
 status = true;
 
+% ensure that our "random" matrices are reproducible to aide in debugging.
+%
+% XXX: this doesn't actually work.
 rand( 'seed', 1 );
 
+% order of the matrices we'll use for this test.
 n = 5;
 
+% double and single precision complex and real matrices.
 X_double = randn( n ) + randn( n ) * i;
 Y_double = real( X_double );
-
-Y_int8   = int8( Y_double );
-Y_int16  = int16( Y_double );
-Y_int32  = int32( Y_double );
-Y_int64  = int64( Y_double );
 
 X_single = single( X_double );
 Y_single = single( Y_double );
 
-% NOTE: we compute a positive definite, Hermitian matrix in single and double
-%       precision and then cast it to integral data types.  this works around
-%       the fact that Octave doesn't support matrix operations on integral
-%       data.
+% create full rank, positive definite, Hermitian and symmetric matrices for
+% all data types we're going to test.
+%
+% NOTE: we cast our Hermitian and symmetric to work around the fact that
+%       Octave doesn't support matrix operations on integral data.
+%
 R_single = X_single * X_single';
 R_double = X_double * X_double';
 S_single = Y_single * Y_single';
@@ -43,69 +47,84 @@ S_double = Y_double * Y_double';
 S_int8   = int8( S_double );
 S_int16  = int16( S_double );
 S_int32  = int32( S_double );
+S_int64  = int64( S_double );
 
-% calling without parameters is invalid.
-try
-    cholesky_inverse;
-
-    status = false;
-    error( 'cholesky_inverse() executed without any parameters.' );
-catch
-end
+% begin testing the MEX object.
 
 % test cases for native precision using Hermitian and symmetric matrices.
-status = and( status, run_test( @() verify_inversion( R_single ), ...
-                                'Failed to execute with a single precision, Hermitian matrix.' ) );
-status = and( status, run_test( @() verify_inversion( R_double ), ...
-                                'Failed to execute with a double precision, Hermitian matrix.' ) );
-status = and( status, run_test( @() verify_inversion( S_single ), ...
-                                'Failed to execute with a single precision, symmetric matrix.' ) );
-status = and( status, run_test( @() verify_inversion( S_double ), ...
-                                'Failed to execute with a single precision, symmetric matrix.' ) );
+status = and( status, test_success( @() verify_inversion( R_single ), ...
+                                    'Failed to execute with a single precision, Hermitian matrix.' ) );
+status = and( status, test_success( @() verify_inversion( R_double ), ...
+                                    'Failed to execute with a double precision, Hermitian matrix.' ) );
+status = and( status, test_success( @() verify_inversion( S_single ), ...
+                                    'Failed to execute with a single precision, symmetric matrix.' ) );
+status = and( status, test_success( @() verify_inversion( S_double ), ...
+                                    'Failed to execute with a single precision, symmetric matrix.' ) );
 
 % test cases for specified precision using Hermitian matrices.
-status = and( status, run_test( @() verify_inversion( R_single, 'single' ), ...
-                                'Failed to execute with a single precision, Hermitian matrix, specifying single precision.' ) );
-status = and( status, run_test( @() verify_inversion( R_double, 'single' ), ...
-                                'Failed to execute with a double precision, Hermitian matrix, specifying single precision.' ) );
-status = and( status, run_test( @() verify_inversion( R_single, 'double' ), ...
-                                'Failed to execute with a single precision, Hermitian matrix, specifying double precision.' ) );
-status = and( status, run_test( @() verify_inversion( R_double, 'double' ), ...
-                                'Failed to execute with a double precision, Hermitian matrix, specifying double precision.' ) );
+status = and( status, test_success( @() verify_inversion( R_single, 'single' ), ...
+                                    'Failed to execute with a single precision, Hermitian matrix, specifying single precision.' ) );
+status = and( status, test_success( @() verify_inversion( R_double, 'single' ), ...
+                                    'Failed to execute with a double precision, Hermitian matrix, specifying single precision.' ) );
+status = and( status, test_success( @() verify_inversion( R_single, 'double' ), ...
+                                    'Failed to execute with a single precision, Hermitian matrix, specifying double precision.' ) );
+status = and( status, test_success( @() verify_inversion( R_double, 'double' ), ...
+                                    'Failed to execute with a double precision, Hermitian matrix, specifying double precision.' ) );
 
 % test cases for specified precision using symmetric matrices.
-status = and( status, run_test( @() verify_inversion( S_single, 'single' ), ...
-                                'Failed to execute with a single precision, symmetric matrix, specifying single precision.' ) );
-status = and( status, run_test( @() verify_inversion( S_double, 'single' ), ...
-                                'Failed to execute with a double precision, symmetric matrix, specifying single precision.' ) );
-status = and( status, run_test( @() verify_inversion( S_single, 'double' ), ...
-                                'Failed to execute with a single precision, symmetric matrix, specifying double precision.' ) );
-status = and( status, run_test( @() verify_inversion( S_double, 'double' ), ...
-                                'Failed to execute with a double precision, symmetric matrix, specifying double precision.' ) );
+status = and( status, test_success( @() verify_inversion( S_single, 'single' ), ...
+                                    'Failed to execute with a single precision, symmetric matrix, specifying single precision.' ) );
+status = and( status, test_success( @() verify_inversion( S_double, 'single' ), ...
+                                    'Failed to execute with a double precision, symmetric matrix, specifying single precision.' ) );
+status = and( status, test_success( @() verify_inversion( S_single, 'double' ), ...
+                                    'Failed to execute with a single precision, symmetric matrix, specifying double precision.' ) );
+status = and( status, test_success( @() verify_inversion( S_double, 'double' ), ...
+                                    'Failed to execute with a double precision, symmetric matrix, specifying double precision.' ) );
+
+% calling without parameters is invalid.
+status = and( status, test_failure( @() cholesky_inverse, ...
+                                    'MEX object allowed no arguments to be supplied.' ) );
+
+% calling with invalid parameters is invalid.  first supply matrices with
+% integral data types, then supply invalid precision strings.
+for input_matrix = { S_int8, S_int16, S_int32, S_int64 }
+    status = and( status, test_failure( @() cholesky_inverse( input_matrix{1} ), ...
+                                        'MEX object should not allow inputs that are not single- or double precision.' ) );
+end
+
+for type_str = { 'int8', 'int16', 'int32', 'SINGLE', 'DOUBLE', 'FLOAT32', 'FLOAT64' }
+    status = and( status, test_failure( @() cholesky_inverse( R_single, type_str{1} ), ...
+                                        sprintf( 'MEX object should not allow computation precision ''%s''.', ...
+                                                 type_str{1} ) ) );
+end
 
 if status
     disp( 'All passed!' )
 end
 
-keyboard
-
-% calling with invalid parameters is invalid.
-for type_str = { 'int8', 'int16', 'int32', 'SINGLE', 'DOUBLE', 'FLOAT32', 'FLOAT64' }
-  try
-      cholesky_inverse( R_single, type_str{1} );
-
-      status = false;
-  catch
-  end
-end
-
 return
 
 function verify_inversion( R, precision )
-% XXX: the tolerances here are awfully large.  should be set based on the
-%      class and the norm or R.
-
-I = eye( size( R ), class( R ) );
+% verify_inversion( R, precision )
+%
+% Verifies that the supplied positive definite matrix can be inverted using
+% the Cholesky inversion MEX object.  The inverse is compared against the
+% inverse computed via back-substitution against the Cholesky factorization,
+% as well as how well it computes the identify from the original matrix.
+%
+% This function makes several calls to assert() to verify that the MEX
+% object's inversion is correct.  If a problem occurs during execution, this
+% function throws an error and does not return.
+%
+% Takes 2 arguments:
+%
+%   R         - Positive definite matrix (Hermitian or symmetric) to invert
+%               using Cholesky decomposition.
+%   precision - String indicating the precision of the inversion computation.
+%               Must be one of the valid strings accepted by
+%               cholesky_inverse().
+%
+% Returns nothing.
 
 % invert using the MEX object.
 if nargin < 2
@@ -116,10 +135,13 @@ end
 
 % invert using the built-in Cholesky decomposition routine and
 % back-substitution.
+I     = eye( size( R ), class( R ) );
 U     = chol( R );
 U_inv = U \ I;
 R_inv = U_inv * U_inv';
 
+% XXX: the tolerances here are awfully large.  should be set based on the
+%      class and the norm or R.
 close_enough = norm( (R_inv_mex * R) - I ) < 5e-4;
 assert( close_enough );
 
@@ -128,42 +150,168 @@ assert( close_enough );
 
 return
 
-function status = run_test( function_handle, error_message )
+function status = test_success( function_handle, error_message )
+% status = test_success( function_handle, error_message )
+%
+% Evaluates the supplied anonymous function and verifies that it successfully
+% executes.  If execution fails, displays the supplied error message and returns
+% false.  Otherwise, returns true.
+%
+% Takes 2 arguments:
+%
+%   function_handle - Function handle, that requires no arguments or output
+%                     values, to evaluate for success using feval().
+%   error_message   - Error message to display if the function evaluation
+%                     fails.
+%
+% Returns 1 value:
+%
+%   status - Logical indicating whether the function execution succeeded
+%            (true), or failed (false).
 
+status = run_test( function_handle, error_message, true );
+
+return
+
+function status = test_failure( function_handle, error_message )
+% status = test_success( function_handle, error_message )
+%
+% Evaluates the supplied anonymous function and verifies that it fails to
+% execute.  If execution succeeds, displays the supplied error message and
+% returns false.  Otherwise, returns true.
+%
+% Takes 2 arguments:
+%
+%   function_handle - Function handle, that requires no arguments or output
+%                     values, to evaluate for failure using feval().
+%   error_message   - Error message to display if the function evaluation
+%                     succeeds.
+%
+% Returns 1 value:
+%
+%   status - Logical indicating whether the function execution failed (true),
+%            or succeeded (false).
+
+status = run_test( function_handle, error_message, false );
+
+return
+
+function status = run_test( function_handle, error_message, expected_result )
+% status = run_test( function_handle, error_message, expected_result )
+%
+% Runs a function handle, typically anonymous, and compares the execution
+% results against the specified expected result.  Prints the error message to
+% standard output and returns failure (false) if the function did not behave
+% as expected, or returns success (true) if it did.
+%
+% Takes 3 arguments:
+%
+%   function_handle - Function handle, that requires no arguments or output values,
+%                     to evaluate using feval().
+%   error_message   - Error message to display if the function evaluation does not
+%                     behave according to expected_result.
+%   expected_result - Optional logical specifying whether the test should pass
+%                     (true) or fail (false).  If omitted, defaults to true.
+%
+% Returns 1 value:
+%
+%   status - Logical indicating whether the test behaved according to
+%            expected_result.  True if the test executed according to
+%            expected_result, false otherwise.
+
+if nargin < 3
+    expected_result = [];
+end
+
+if isempty( expected_result )
+    expected_result = true;
+end
+
+% we build a failure message from an error stack trace under Octave.  identify
+% the "bottom" of the error stack that we wish to print out.
+if expected_result == true
+    stack_index = 3;
+else
+    % XXX: this is likely wrong as it hasn't been thoroughly tested.
+    stack_index = 3;
+end
+
+% we start out assuming this test has passed.
 status = true;
 
-if is_octave
-    try
-        feval( function_handle );
-    catch
-        disp( error_message );
-        err = lasterror;
-        disp( sprintf( 'Test failed - %s\n%s:%d -> %s:%d\n', ...
-                       err.message, ...
-                       err.stack(4).file, err.stack(4).line, ...
-                       err.stack(2).file, err.stack(2).line ) );
-        status = false;
+if expected_result == true
+    % we expect our try/catch block to execute without problems.  set the
+    % status to failure if we get into the catch, and report where we went
+    % awry.
+    if is_octave
+        try
+            feval( function_handle );
+        catch
+            disp( error_message );
+            err = lasterror;
+            disp( sprintf( 'Test failed - %s\n%s:%d -> %s:%d\n', ...
+                           err.message, ...
+                           err.stack(stack_index).file, err.stack(stack_index).line, ...
+                           err.stack(stack_index-2).file, err.stack(stack_index-2).line ) );
+            status = false;
+        end
+    else
+        try
+            feval( function_handle );
+        catch me
+            disp( error_message );
+            getReport( me )
+            status = false;
+        end
     end
 else
-    try
-        feval( function_handle );
-    catch me
-        disp( error_message );
-        getReport( me )
-        status = false;
+    % we expect our try/catch block to fail.  set the status to failure if we
+    % get past the feval(), and report where we went awry.
+    if is_octave
+        try
+            feval( function_handle );
+            status = false;
+            disp( sprintf( 'Test executed when it should have failed - %s\n%s:%d -> %s:%d\n', ...
+                           err.message, ...
+                           err.stack(4).file, err.stack(4).line, ...
+                           err.stack(2).file, err.stack(2).line ) );
+        catch
+        end
+    else
+        try
+            feval( function_handle );
+            disp( error_message );
+            getReport( me )
+            status = false;
+        catch me
+        end
     end
 end
 
 return
 
 function flag = is_octave()
+% flag = is_octave()
+%
+% Predicate indicating whether the interpreter is Octave.  Returns true if so,
+% false if running within MATLAB.
+%
+% Takes no arguments.
+%
+% Returns 1 value:
+%
+%   flag - Logical indicating whether execution occurs within Octave.  True if
+%          so.  False otherwise.
 
+% keep the result of our check in memory since this function won't change it's
+% return value once it's been determined.
 persistent existence_result
 
 if isempty( existence_result )
    existence_result = exist( 'OCTAVE_VERSION', 'builtin' );
 end
 
-flag = existence_result ~= 0;
+% we're running under Octave if OCTAVE_VERSION is a builtin.
+flag = existence_result == 5;
 
 return
