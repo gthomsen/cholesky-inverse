e7a5de02d02251c166c558914980833d79e17639
Greg Thomsen <gthomsen@gmail.com>
2015-08-14 23:17:07 -0500

    Topic: Cholesky Inverse MEX Object Help Update Description:   * Cleaned up the Cholesky Inverse MEX object's help function.  Now the help     accurately describes what the object does as well as the parameters and     return values it accepts.

--- cholesky_inverse.m
+++ cholesky_inverse.m
@@ -1,22 +1,29 @@
 function X_inverse = cholesky_inverse( X, precision )
 % X_inverse = cholesky_inverse( X, precision )
 %
-% XXX: computes the inverse of X using Cholesky decomposition.
+% Computes the inverse of X using Cholesky decomposition.  The precision the
+% inversion is performed in may be specified so as to reduce numerical error
+% in the process.  If the supplied matrix is not positive definite, an error
+% is thrown.
 %
-% Takes 2 argument:
+% Takes 2 arguments:
 %
 %   X         - Positive definite matrix to invert using Cholesky
 %               decomposition.
-%   precision - String indicating the precision that the inversion should
-%               performed in.  The inverted X has the same precision as
-%               the supplied X.
 %
-% input/output  float    double
-%    float       yes       yes
-%    double      no        yes
+%   precision - Optional string indicating the precision that the inversion
+%               should performed in.  Must be one of the following:
+%
+%                 'double', 'float64' -  Double precision.
+%                 'single', 'float32' -  Single precision.
+%
+%               If omitted, inversion is performed in the same precision as X.
+%
+%               NOTE: This only affects the precision used for inversion and
+%                     not the precision of the inverse returned.
 %
 % Returns 1 value:
 %
-%   X_inverse -
+%   X_inverse - Inverse of X.
 
 return
